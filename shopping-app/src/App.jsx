00ca7b2cbd31751d7188737d325dc1cf886c0401
mehmetnuribasa <basamehmet394@gmail.com>
2025-04-02 00:22:22 +0300

    Initial commit

---
+++ shopping-app/src/App.jsx
@@ -0,0 +1,37 @@
+// rafce -> create the componet with file name automatically.
+
+import React from 'react'
+
+const App = () => {
+  return (
+  <main className=''>
+    Nav
+    <section className=' px-8 p-12 m-8 bg-amber-200'>
+      Hero
+    </section>
+    <section className=''>
+      PopularProducts
+    </section>
+    <section className=''>
+      SuperQuality
+    </section>
+    <section className=''>
+      Services
+    </section>
+    <section className=''>
+      SpecialOffer
+    </section>
+    <section className='bg-blue-200 padding'>
+      CustomerReviews
+    </section>
+    <section className=''>
+      Subscribe
+    </section>
+    <section className=''>
+      Footer
+    </section>
+  </main>
+  );
+}
+
+export default App;
